d76c5472bd4c515c2955c6f63ffa2787424548b9
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2025-08-26 08:36:45 -0300

    gg

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ geraSenha();
 function geraSenha (){
     let senha = '';
     for (let i=0; i<tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*length;
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
